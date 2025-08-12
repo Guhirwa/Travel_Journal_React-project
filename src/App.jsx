457db8bbc37915fa55f9c7eb1a85edf7e90c005b
Guhirwa <guhirwachristian004@gmail.com>
2025-08-12 23:12:37 +0200

    Finishing Mount Fuji card#1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './assets/components/Header'
+import JournarEntry  from './assets/components/JournarEntry'
 
 const App = () => {
   return (
     <>
       <Header />
+      <JournarEntry />
     </>
   )
 }
