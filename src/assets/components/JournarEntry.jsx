0704134029098a12ffb551b8156d5e6fb831fd83
Guhirwa <guhirwachristian004@gmail.com>
2025-08-13 23:40:52 +0200

    Creating reusable components - I used props to create a reusable components. - The child component will be having the general structure only allowing it to be frexible to receive any   kind of that but with the same structure. - the parent component will be the one to add and differnciate data at every child component. - These were made possible by the help of React props (Properties)

--- src/assets/components/JournarEntry.jsx
+++ src/assets/components/JournarEntry.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 
-const JournarEntry = () => {
+const JournarEntry = (props) => {
   return (
     <article className = 'flex mx-auto w-2/5 gap-6 mt-6'>
         <div className = 'w-40 h-52 shrink-0'>
-            <img className = 'h-full w-full bg-cover rounded-md' src="/src/assets/images/Mount_Fuji.jpg" alt="mount fuji" />
+            <img className = 'h-full w-full bg-cover rounded-md' src={props.mountFuji.src} alt = {props.mountFuji.alt} />
         </div>
         <div> 
             <div className = 'flex'>
-                <img src="/src/assets/images/location_icon.svg" alt="map marker icon" />
+                <img src = {props.mapIcon.src} alt = {props.mapIcon.alt} />
                 <span className = 'font-medium'>JAPAN</span>
-                <a className = 'ml-3 text-gray-600 underline' target = '_blank' href="https://maps.app.goo.gl/FJpfbrPHGJ4aZNxN8">View on Google Map</a>
+                <a className = 'ml-3 text-gray-600 underline' target = {props.mapLink.target} href = {props.mapLink.href} >{props.mapLink.text}</a>
             </div>
-            <h2 className = 'font-bold my-2 text-2xl'>Mount Fuji</h2>
-            <p className = 'font-bold  my-3'>12 Jan, 2025 - 24 Sept 2025</p>
+            <h2 className = 'font-bold my-2 text-2xl'>{props.mountName}</h2>
+            <p className = 'font-bold  my-3'>{props.date}</p>
             <p className = 'font-light'>
-                Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet)
-                Mount Fuji is the single most poular tourist site in Japan, for both Japanese and foreign tourists.
+               {props.generalText}
             </p>
         </div>
 
