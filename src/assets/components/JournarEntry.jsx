50cebb53b4998bdfe09cc4f5a35963e4c1a1ffcc
Guhirwa <guhirwachristian004@gmail.com>
2025-08-19 06:40:22 +0200

    Tranform to data-driven App - I created a single js file with all data needed in form of array of object. - Using mapping I traverse throught each single object from that array and mapp its data to a single entry journal component. - For my reusable component for every new data new unique component get createad

--- src/assets/components/JournarEntry.jsx
+++ src/assets/components/JournarEntry.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
+import locationIcon from '/src/assets/images/location_icon.svg'
 
 const JournarEntry = (props) => {
   return (
-    <article className = 'flex mx-auto w-2/5 gap-6 mt-6'>
+    <article className = 'flex mx-auto w-2/5 gap-6 my-10'>
         <div className = 'w-40 h-52 shrink-0'>
-            <img className = 'h-full w-full bg-cover rounded-md' src={props.mountFuji.src} alt = {props.mountFuji.alt} />
+            <img className = 'h-full w-full bg-cover rounded-md' src={props.img.src} alt = {props.img.alt} />
         </div>
         <div> 
-            <div className = 'flex'>
-                <img src = {props.mapIcon.src} alt = {props.mapIcon.alt} />
-                <span className = 'font-medium'>{props.countryName}</span>
-                <a className = 'ml-3 text-gray-600 underline' target = {props.mapLink.target} href = {props.mapLink.href} >{props.mapLink.text}</a>
+            <div className = 'flex items-center'>
+                <img className = 'h-auto w-5 mr-3' src = {locationIcon} alt = "location Icon" />
+                <span className = 'font-medium'>{props.country}</span>
+                 <a className = 'ml-3 text-gray-600 underline' target = '_blank' href = {props.googleMapsLink} >View on Google Maps</a>
             </div>
-            <h2 className = 'font-bold my-2 text-2xl'>{props.mountName}</h2>
-            <p className = 'font-bold  my-3'>{props.date}</p>
-            <p className = 'font-light'>
-               {props.generalText}
-            </p>
+            <h2 className = 'font-bold my-2 text-2xl'>{props.title}</h2>
+            <p className = 'font-bold  my-3'>{props.dates}</p>
+            <p className = 'font-light'>{props.text}</p>
         </div>
-
     </article>
   )
 }
